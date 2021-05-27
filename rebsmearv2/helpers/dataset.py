8c83c382b74234f33ffd9ea4a3313b02e546cdf2
Alp Akpinar <aakpinar@bu.edu>
2021-05-27 13:18:25 -0500

    Point to the samples without filtering

--- rebsmearv2/helpers/dataset.py
+++ rebsmearv2/helpers/dataset.py
@@ -54,7 +54,7 @@ def files_from_eos(regex):
     :rtype: dict
     '''
     topdir = '/eos/uscms/store/user/aakpinar/nanopost'
-    tag = 'ULv8_05Feb21'
+    tag = 'ULv8_05Feb21_nofilter'
     fileset = find_files_eos(pjoin(topdir, tag), regex)
     
     return fileset
