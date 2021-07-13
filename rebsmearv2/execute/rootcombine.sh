b1ff7a3404c607a3940b694c24b810d8324508fc
Alp Akpinar <aakpinar@bu.edu>
2021-07-13 13:21:21 -0500

    Script to combine root files via hadd

---
+++ rebsmearv2/execute/rootcombine.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Combine the ROOT trees in the specified output directory via hadd command.
+INDIR=${1}
+
+TARGETDIR=${INDIR}/combined
+
+pushd ${TARGETDIR}
+hadd JetHT_rebalanced_combined.root ../JetHT_*rebalanced_tree*.root
+popd
